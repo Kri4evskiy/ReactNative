8dce6efa328352705ee7d4c0d5d0c54ebb9a1f8c
Kri4evskiy <kri4evskiy@gmail.com>
2020-07-05 16:29:42 +0300

    first

---
+++ First-RN/App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello React Native</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  text: {
+    color: '#fff',
+    fontSize: 26
+  }
+})
