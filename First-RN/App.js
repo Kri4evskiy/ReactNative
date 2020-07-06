814b43c9830f53c7c48038bb76164cd8b54ed111
Kri4evskiy <kri4evskiy@gmail.com>
2020-07-06 14:22:32 +0300

    Practice

--- First-RN/App.js
+++ First-RN/App.js
@@ -1,25 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { Navbar } from './src/Navbar'
+import { AddTodo } from './src/AddTodo'
+import { Todo } from './src/Todo';
 
 
 export default function App() {
+  const [todos, setTodos] = useState([])
+
+  const addTodo = (title) => {
+    setTodos(prev => [
+      ...prev,
+      {
+        id: Date.now().toString(),
+        title
+      }])
+  }
+
+  const removeTodo = id => {
+    setTodos(prev => prev.filter(todo => todo.id !== id))
+  }
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello React Native</Text>
+    <View>
+      <Navbar title='Todo App' />
+      <View style={styles.container}>
+        <AddTodo onSubmit={addTodo} />
+
+        <FlatList
+          keyExtractor={item => item.id.toString()}
+          data={todos}
+          renderItem={({ item }) => (
+            <Todo todo={item} onRemove={removeTodo}/>
+          )}
+        />
+      </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  text: {
-    color: '#fff',
-    fontSize: 26
+    paddingVertical: 20,
+    paddingHorizontal: 30
   }
 })
