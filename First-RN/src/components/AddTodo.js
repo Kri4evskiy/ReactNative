b26b4bedba9f67656b0fb63802a7c1e455e68b8f
Kri4evskiy <kri4evskiy@gmail.com>
2020-07-09 18:41:42 +0300

    .

--- First-RN/src/components/AddTodo.js
+++ First-RN/src/components/AddTodo.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { View, StyleSheet, TextInput, Button, Alert, Keyboard } from 'react-native'
+import { View, StyleSheet, TextInput, Alert, Keyboard } from 'react-native'
 import { AntDesign } from '@expo/vector-icons'
 import { THEME } from '../theme'
 
